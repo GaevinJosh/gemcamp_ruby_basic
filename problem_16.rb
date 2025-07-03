371c479d33039db4dec27c127b27a3ef2b39e12b
GaevinJosh <gaevinjosha@gmail.com>
2025-07-03 16:42:57 +0800

    three string

---
+++ problem_16.rb
@@ -0,0 +1,10 @@
+def link_three_string
+  name = "Gaevin"
+  hobby = "scrolling thru phone"
+  color = "Blue"
+  combine = name + " " + hobby + " " + color
+
+  puts combine
+end
+
+link_three_string
